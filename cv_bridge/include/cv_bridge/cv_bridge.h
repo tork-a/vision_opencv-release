ffb3e033c902afcd26e009d86fa2414ee902e292
Vincent Rabaud <vincent.rabaud@gmail.com>
2016-01-16 15:21:06 +0100

    Rebase from 'upstream'

--- cv_bridge/include/cv_bridge/cv_bridge.h
+++ cv_bridge/include/cv_bridge/cv_bridge.h
@@ -258,11 +258,14 @@ CvImagePtr cvtColor(const CvImageConstPtr& source,
  *
  * The following rules apply:
  * - if the output encoding is empty, the fact that the input image is mono or multiple-channel is
- * preserved in the ouput image. The bit depth will be 8.
+ * preserved in the ouput image. The bit depth will be 8. it tries to convert to BGR no matter what
+ * encoding image is passed.
  * - if the output encoding is not empty, it must have sensor_msgs::image_encodings::isColor and
  * isMono return true. It must also be 8 bit in depth
  * - if the input encoding is an OpenCV format (e.g. 8UC1), and if we have 1,3 or 4 channels, it is
  * respectively converted to mono, BGR or BGRA.
+ * - if the input encoding is 32SC1, this estimate that image as label image and will convert it as
+ * bgr image with different colors for each label.
  *
  * \param source   A shared_ptr to a sensor_msgs::Image message
  * \param encoding Either an encoding string that returns true in sensor_msgs::image_encodings::isColor
